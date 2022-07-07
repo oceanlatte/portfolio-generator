fdbe9de9bf4bfd4f25664b3403924975ce3e5ec2
ocean <osciris.veliz@gmail.com>
2022-07-06 22:14:02 -0500

    add html template

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-}
+const generatePage = (userName, githubName) => {
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" contenet="ie-edge">
+    <title>Portfolio Demo</title>
+  </head>
 
-printProfileData(profileDataArgs);
+  <body>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${github}">GitHub</a></h2>
+  </body>
+  </html>
+  `;
+};
+
+console.log(name, github);
+console.log(generatePage(name, github));
