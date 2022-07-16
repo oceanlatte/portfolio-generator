2610c8d5fd25d0eced38c6fe193357eb6d818c6d
ocean <osciris.veliz@gmail.com>
2022-07-15 21:08:41 -0500

    get projects to generate on index.html

--- app.js
+++ app.js
@@ -49,26 +49,21 @@ const promptUser = () => {
           return false;
         }
       }
-    },
-    {
-      type: 'input',
-      name: 'about',
-      message: 'Provide some information about yourself:'
     }
   ]);
 };
 
 const promptProject = portfolioData => {
-  //If There's no 'projects' array property, create one
-  if (!portfolioData.projects) {
-    portfolioData.projects = [];
-  }
-
-  console.log(`
+    console.log(`
   =================
   Add a New Project
   =================
   `);
+
+  //If There's no 'projects' array property, create one
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
   return inquirer.prompt([
     {
       type: 'input',
@@ -142,8 +137,48 @@ const promptProject = portfolioData => {
 const mockData = {
   name: 'Lernantino',
   github: 'lernantino',
-  projects: []
-};
+  confirmAbout: true,
+  about:
+      'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.',
+  projects: [
+      {
+        name: 'Run Buddy',
+        description:
+          'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+        languages: ['HTML', 'CSS'],
+        link: 'https://github.com/lernantino/run-buddy',
+        feature: true,
+        confirmAddProject: true
+      },
+      {
+        name: 'Taskinator',
+        description:
+          'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+        languages: ['JavaScript', 'HTML', 'CSS'],
+        link: 'https://github.com/lernantino/taskinator',
+        feature: true,
+        confirmAddProject: true
+      },
+      {
+        name: 'Taskmaster Pro',
+        description:
+          'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+        languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
+        link: 'https://github.com/lernantino/taskmaster-pro',
+        feature: false,
+        confirmAddProject: true
+      },
+      {
+        name: 'Robot Gladiators',
+        description:
+          'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.',
+        languages: ['JavaScript'],
+        link: 'https://github.com/lernantino/robot-gladiators',
+        feature: false,
+        confirmAddProject: false
+      }
+    ]
+  };
 
 const pageHTML = generatePage(mockData);
 
@@ -152,8 +187,8 @@ const pageHTML = generatePage(mockData);
 //   .then(portfolioData => {
 //     const pageHTML = generatePage(portfolioData);
 
-//     fs.writeFile('./index.html', pageHTML, err => {
-//       if (err) throw err;
-//       console.log('Portfolio complete! Check out index.html to see the output!');
-//     });
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw err;
+      console.log('Portfolio complete! Check out index.html to see the output!');
+    });
 //   });
--- src/page-template.js
+++ src/page-template.js
@@ -1,7 +1,45 @@
+// about section function
+const generateAbout = aboutText => {
+  if (!aboutText) {
+    return '';
+  }
+
+  return `
+    <section class="my-3" id="about">
+      <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+      <p>${aboutText}</p>
+    </section>
+  `
+};
+
+const generateProjects = projectsArr => {
+  const projectHtmlArr = projectsArr.map(({ name, description, languages, link }) => {
+    return ` 
+      <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+        <h3 class="porfolio-item-title text-light">${name}</h3>
+        <h5 class="portfolio-languages">
+          Built With:
+          ${languages.join(', ')}
+        </h5>
+        <p>${description}</p>
+        <a href=${link}" class-"btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+      </div>
+    `;
+  });
+
+  return `
+    <section class="my-3" id="portfolio">
+      <h2 class="flex-row justify-space-between">Work</h2>
+      <div class="flex-row justify-space-between">
+        ${projectHtmlArr.join('')}
+      </div>
+    </section>
+  `;
+};
+
 module.exports = templateData => {
   // descructure projects and about data from templateData based on their property key names
-  const { projects, about, ...header} = templateData;
-  console.log(projects, about, header)
+  const { projects, about, ...header } = templateData;
 
   return `
   <!DOCTYPE html>
@@ -11,12 +49,28 @@ module.exports = templateData => {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" contenet="ie-edge">
     <title>Portfolio Demo</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
   </head>
 
   <body>
-    <h1>${templateData.name}</h1>
-    <h2><a href="https://github.com/${templateData.github}">GitHub</a></h2>
+    <header>
+      <div class="container flex-row justify-space-between align-center py-3">
+        <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
+        <nav class="flex-row">
+          <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github}">GitHub</a>
+        </nav>
+      </div>
+    </header>
+    <main class="container my-5">
+      ${generateAbout(about)}
+      ${generateProjects(projects)}
+    </main>
+    <footer class="container text-center py-3">
+      <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
+    </footer>
   </body>
   </html>
-  `;
+  `; 
 }; 
