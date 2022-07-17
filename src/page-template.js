763eacc9ceec14fd33551ff838fafb35a1e30a80
ocean <osciris.veliz@gmail.com>
2022-07-17 10:49:44 -0500

    add stylesheet and new destination folder

--- src/page-template.js
+++ src/page-template.js
@@ -20,7 +20,7 @@ const generateProjects = projectsArr => {
       <div class="flex-row justify-space-between">
        ${projectsArr
       .filter(({ feature }) => feature)
-      .map(({ name, description, languages, linl }) => {
+      .map(({ name, description, languages, link }) => {
         return `
           <div class="col-12 mb-2 bg-dark text-light p-3 flex-column">
             <h3 class="porfolio-item-title text-light">${name}</h3>
@@ -29,7 +29,7 @@ const generateProjects = projectsArr => {
               ${languages.join(', ')}
             </h5>
             <p>${description}</p>
-            <a href=${link}" class-"btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+            <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
           </div>
         `
       })
@@ -46,7 +46,7 @@ const generateProjects = projectsArr => {
             ${languages.join(', ')}
           </h5>
           <p>${description}</p>
-          <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Portfolio on GitHub</a>
+          <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
         </div>
         `;
       })
